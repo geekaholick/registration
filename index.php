6ecf77b36f5c2f58a59836d84c5b746a64e52630
Katteu <katheamari@gmail.com>
2023-08-14 04:12:57 +0800

    [update] localized bootstrap and jquery

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.13.3/datatables.min.css" rel="stylesheet"/>
+    <link href="bootstrap-5.3.1/bootstrap.min.css" rel="stylesheet">
+    <link href="DataTables/datatables.min.css" rel="stylesheet"/>
 
     <title>USC DCISM Attendance System</title>
   </head>
@@ -136,9 +136,10 @@
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.13.3/datatables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="bootstrap-5.3.1/jquery-3.7.0.min.js"></script>
+    <script src="DataTables/datatables.min.js"></script>
+    <script src="bootstrap-5.3.1/js/bootstrap.bundle.min.js"></script>
+    
     <script type="text/javascript">
         $('#datatable').DataTable({
             'serverSide': true,
