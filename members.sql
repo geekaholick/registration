3acb3cbb8b92fb7c76661aeab526eeacfff66301
Katteu <katheamari@gmail.com>
2023-08-14 19:41:30 +0800

    updated sql

--- members.sql
+++ members.sql
@@ -11,15 +11,8 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `uscdcism`
---
+CREATE DATABASE uscdcism;
+USE uscdcism;
 
 -- --------------------------------------------------------
 
